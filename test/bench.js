1746127abfefd4047e98792358847efa53470f8c
Rafał Michalski <royaltm75@gmail.com>
2015-01-23 01:36:35 +0100

    test: bench vs crypto + string encoding argument

--- test/bench.js
+++ test/bench.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
-var ben       = require('ben')
-,   hash      = require('..')
-,   assert    = require('assert')
-,   iters     = 100000
-;
+var ben            = require('ben')
+,   hash           = require('..')
+,   assert         = require('assert')
+,   iters          = 100000
+,   createHash     = require('crypto').createHash
+,   stringEncoding = process.argv[2]
+
+if (stringEncoding)
+  console.log('string encoding: %s', stringEncoding);
+
+function hasher(name, data, encoding) {
+  var sum = createHash(name);
+  sum.update(data, encoding);
+  return sum.digest();
+}
+
+function md5(data, encoding) { return hasher('md5', data, encoding) }
+function sha1(data, encoding) { return hasher('sha1', data, encoding) }
 
 var functs = [
   [hash.murmurHash,       'murmurHash      '],
@@ -11,32 +24,46 @@ var functs = [
   [hash.murmurHash64x64,  'murmurHash64x64 '],
   [hash.murmurHash128x86, 'murmurHash128x86'],
   [hash.murmurHash128x64, 'murmurHash128x64'],
+  [md5,                   'md5             '],
+  [sha1,                  'sha1            '],
 ];
+
+function fillrandom(buffer) {
+  for(var i = 0; i < buffer.length; ++i)
+    buffer[i] = (Math.random()*0x100)|0;
+  return buffer;
+}
+
+function randomstring(length) {
+  var s = '';
+  for(var i = 0; i < length; ++i)
+    s += String.fromCharCode((Math.random()*0x100)|0);
+  return s;
+}
 
 functs.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 80
-    , string = new Buffer(size).toString('utf8');
-  measure("string", fun, name, iters*50, size, string);
+    , string = randomstring(size);
+  measure("string", fun, name, iters*5, size, string);
 })
 
 functs.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 16384
-    , string = new Buffer(size).toString('utf8');
+    , string = randomstring(size);
   measure("string", fun, name, iters, size, string);
 })
 
 functs.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 16384
-    , buffer = new Buffer(size);
-  measure("buffer", fun, name, iters*10, size, buffer);
+    , buffer = fillrandom(new Buffer(size));
+  measure("buffer", fun, name, iters, size, buffer);
 })
-
 
 function measure(label, fun, name, iters, size, arg) {
   ben(iters, function(){ fun(arg) }); // warm-up
-  var ms = ben(iters, function(){ fun(arg) });
+  var ms = ben(iters, function(){ fun(arg, stringEncoding, 0) });
   console.log(name + "(" + label + "[" + size + "]): %s", ((1 / ms / 1000) * size).toFixed(4) + 'MB/s');
 }
