6ed4d583f2d762f4873941cb328aa24715713e2c
Rafał Michalski <royaltm75@gmail.com>
2015-01-24 02:14:59 +0100

    bench: add all crypto hashes

--- test/bench.js
+++ test/bench.js
@@ -2,31 +2,35 @@
 var ben            = require('ben')
 ,   hash           = require('..')
 ,   assert         = require('assert')
-,   iters          = 100000
-,   createHash     = require('crypto').createHash
+,   iters          = 10000
+,   crypto         = require('crypto')
+,   createHash     = crypto.createHash
 ,   stringEncoding = process.argv[2]
 
 if (stringEncoding)
   console.log('string encoding: %s', stringEncoding);
 
-function hasher(name, data, encoding) {
+function cryptohasher(name, data, encoding) {
   var sum = createHash(name);
   sum.update(data, encoding);
   return sum.digest();
 }
 
-function md5(data, encoding) { return hasher('md5', data, encoding) }
-function sha1(data, encoding) { return hasher('sha1', data, encoding) }
-
-var functs = [
-  [hash.murmurHash,       'murmurHash      '],
-  [hash.murmurHash64x86,  'murmurHash64x86 '],
-  [hash.murmurHash64x64,  'murmurHash64x64 '],
-  [hash.murmurHash128x86, 'murmurHash128x86'],
-  [hash.murmurHash128x64, 'murmurHash128x64'],
-  [md5,                   'md5             '],
-  [sha1,                  'sha1            '],
+var funmatrix = [
+  [hash.murmurHash,       'murmurHash                    '],
+  [hash.murmurHash64x86,  'murmurHash64x86               '],
+  [hash.murmurHash64x64,  'murmurHash64x64               '],
+  [hash.murmurHash128x86, 'murmurHash128x86              '],
+  [hash.murmurHash128x64, 'murmurHash128x64              '],
 ];
+
+crypto.getHashes().forEach(function(cipher) {
+  var pad = '                              ';
+  funmatrix.push([
+      function(data, encoding) { return cryptohasher(cipher, data, encoding) },
+      cipher + pad.substr(0, pad.length - cipher.length)
+    ]);
+});
 
 function fillrandom(buffer) {
   for(var i = 0; i < buffer.length; ++i)
@@ -41,21 +45,21 @@ function randomstring(length) {
   return s;
 }
 
-functs.forEach(function(args) {
+funmatrix.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 80
     , string = randomstring(size);
   measure("string", fun, name, iters*5, size, string);
 })
 
-functs.forEach(function(args) {
+funmatrix.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 16384
     , string = randomstring(size);
   measure("string", fun, name, iters, size, string);
 })
 
-functs.forEach(function(args) {
+funmatrix.forEach(function(args) {
   var fun = args[0], name = args[1]
     , size = 16384
     , buffer = fillrandom(new Buffer(size));
