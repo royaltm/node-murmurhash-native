e81fb0b91bf3e61b24ec96da405005786581ab45
Rafał Michalski <royaltm75@gmail.com>
2016-05-13 03:09:25 +0200

    test number output compatability with murmurhash3js

---
+++ test/test.compat.js
@@ -0,0 +1,67 @@
+var test = require("tap").test
+  , hash = require('..')
+  , hash3js = require("murmurhash3js")
+;
+
+[
+  {hash3js: hash3js.x86.hash32,  hash: hash.murmurHash32},
+  {hash3js: hash3js.x86.hash128, hash: hash.murmurHash128x86},
+  {hash3js: hash3js.x64.hash128, hash: hash.murmurHash128x64}
+]
+.forEach(function(o) {
+
+  test(o.hash.name + " should be compatible with murmurhash3js", function(t) {
+    t.plan((9 + 10)*(57*3));
+    [
+      "My hovercraft is full of eels.",
+      "I will not buy this tobacconist's, it is scratched.",
+      "Mój poduszkowiec jest pełen węgorzy",
+      "Мээң ховеркрафтым иштинде чылан ышкаш балык долу",
+      "ჩემი ხომალდი საჰაერო ბალიშზე სავსეა გველთევზებით",
+      "私のホバークラフトは鰻でいっぱいです",
+      "എന്‍റെ പറക്കും-പേടകം നിറയെ വ്ളാങ്കുകളാണ്",
+      "මාගේ වායු පා යානයේ ආඳන් පිරී ඇත",
+      ""
+    ]
+    .forEach(testHash);
+
+    for(var i = 10; i-- > 0; ) {
+      var s = '';
+      var len = (1 + Math.random()*10000)|0;
+      for(var n = len; n-- > 0; ) {
+        s += String.fromCharCode(Math.random()*0x3fff);
+      }
+
+      testHash(s);
+    }
+
+    function testStrings(s) {
+      var s1 = new Buffer(s, 'utf8').toString('binary');
+      var args1 = [s1].concat([].slice.call(arguments, 1));
+      var args2 = [s, 'utf-8'].concat(args1.slice(1));
+      t.strictEqual( o.hash3js.apply(o, args1), o.hash.apply(o, args2) );
+      args1[0] = new Buffer(s, 'binary').toString('binary');
+      args2[1] = 'binary';
+      t.strictEqual( o.hash3js.apply(o, args1), o.hash.apply(o, args2) );
+      args2.splice(1, 1);
+      t.strictEqual( o.hash3js.apply(o, args1), o.hash.apply(o, args2) );
+    }
+
+    function testHash(s) {
+      
+      testStrings(s);
+      testStrings(s, 0);
+      testStrings(s, 1);
+      testStrings(s, -1);
+      testStrings(s, 0x7ffffff);
+      testStrings(s, 0x8000000);
+      testStrings(s, 0xfffffff);
+      for(var i = 50; i-- > 0; ) {
+        var seed = (Math.random()*0x100000000)>>>0;
+        testStrings(s, seed);
+      }
+    }
+
+  });
+
+});
