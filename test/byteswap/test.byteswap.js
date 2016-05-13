e91da87b11c7c667d55d495a0720127201afa08c
Rafał Michalski <royaltm75@gmail.com>
2016-05-13 07:14:45 +0200

    test byteswap 64bit

--- test/byteswap/test.byteswap.js
+++ test/byteswap/test.byteswap.js
@@ -7,11 +7,12 @@ var test = require("tap").test
 test("should have murmurHash functions", function(t) {
   testHash(t, hash.murmurHash32, "My hovercraft is full of eels.", 25, 2520298415);
   testHash(t, hash.murmurHash32, "I will not buy this record, it is scratched.", 0, 2832214938);
+  testHash(t, hash.murmurHash64x86, "I will not buy this record, it is scratched.", 0, "455c071b8e33c356");
+  testHash(t, hash.murmurHash64x64, "I will not buy this record, it is scratched.", 0, "9bcc0bed3798d9ea");
   testHash(t, hash.murmurHash128x86, "I will not buy this tobacconist's, it is scratched.", 0, "9b5b7ba2ef3f7866889adeaf00f3f98e");
   testHash(t, hash.murmurHash128x64, "I will not buy this tobacconist's, it is scratched.", 0, "d30654abbd8227e367d73523f0079673");
   t.end();
 });
-
 
 function testHash(t, hash, input, seed, expectation) {
   console.log(input)
@@ -27,10 +28,10 @@ function testHash(t, hash, input, seed, expectation) {
 function swap(value, hash) {
   switch(hash.name) {
     case "murmurHash32":
+    case "murmurHash64x86":
     case "murmurHash128x86":
       return swap32(value);
     case "murmurHash64x64":
-    case "murmurHash64x86":
     case "murmurHash128x64":
       return swap64(value);
     default:
