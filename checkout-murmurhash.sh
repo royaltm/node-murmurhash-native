78fa6e0a83e9a3e91d3df12a8481856725b9b38a
Rafał Michalski <royaltm75@gmail.com>
2016-05-15 00:04:45 +0200

    checkout-murmurhash don't overwrite

--- checkout-murmurhash.sh
+++ checkout-murmurhash.sh
@@ -2,4 +2,14 @@
 
 mkdir -p src/murmurhash
 cd src/murmurhash
-exec curl 'https://raw.githubusercontent.com/aappleby/smhasher/master/src/MurmurHash{2,3}.{cpp,h}' -O -f
+for file in \
+    MurmurHash2.cpp \
+    MurmurHash2.h \
+    MurmurHash3.cpp \
+    MurmurHash3.h
+do
+  if [ ! -e "$file" ]
+  then curl "https://raw.githubusercontent.com/aappleby/smhasher/master/src/$file" -O -f
+  else echo "$file already exists, skipping"
+  fi
+done
