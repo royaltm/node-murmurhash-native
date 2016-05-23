3c0cc988b71eb9251290264bb9ecf88741d3bb86
Rafał Michalski <royaltm75@gmail.com>
2016-05-23 11:25:23 +0200

    test: byteswap incremental

--- test/byteswap/test.byteswap.js
+++ test/byteswap/test.byteswap.js
@@ -6,6 +6,7 @@
 
 var test = require("tap").test
   , hash = require('../..')
+  , incr = require('../../incremental')
 ;
 
 test("should have murmurHash functions", function(t) {
@@ -15,28 +16,36 @@ test("should have murmurHash functions", function(t) {
   testHash(t, hash.murmurHash64x64, "I will not buy this record, it is scratched.", 0, "9bcc0bed3798d9ea");
   testHash(t, hash.murmurHash128x86, "I will not buy this tobacconist's, it is scratched.", 0, "9b5b7ba2ef3f7866889adeaf00f3f98e");
   testHash(t, hash.murmurHash128x64, "I will not buy this tobacconist's, it is scratched.", 0, "d30654abbd8227e367d73523f0079673");
+  testHashIncr(t, incr.MurmurHash, "My hovercraft is full of eels.", 25, 2520298415);
+  testHashIncr(t, incr.MurmurHash, "I will not buy this record, it is scratched.", 0, 2832214938);
+  testHashIncr(t, incr.MurmurHash128x86, "I will not buy this tobacconist's, it is scratched.", 0, "9b5b7ba2ef3f7866889adeaf00f3f98e");
+  testHashIncr(t, incr.MurmurHash128x64, "I will not buy this tobacconist's, it is scratched.", 0, "d30654abbd8227e367d73523f0079673");
   t.end();
 });
 
-function testHash(t, hash, input, seed, expectation) {
-  console.log(input);
-  input = swap(input, hash);
-  console.log(input);
-  if (Buffer.isBuffer(expectation)) {
-    t.deepEqual(hash(input, seed), expectation);
-  } else {
-    t.strictEqual(hash(input, seed), expectation);
-  }
+function testHash(t, murmurHash, input, seed, expectation) {
+  // console.log(input);
+  input = swap(input, murmurHash);
+  // console.log(input);
+  t.strictEqual(murmurHash(input, seed), expectation);
 }
 
-function swap(value, hash) {
-  switch(hash.name) {
-    case "murmurHash32":
-    case "murmurHash64x86":
-    case "murmurHash128x86":
+function testHashIncr(t, MurmurHash, input, seed, expectation) {
+  // console.log(input);
+  input = swap(input, MurmurHash);
+  // console.log(input);
+  t.strictEqual(MurmurHash(seed).update(input).digest('number'), expectation);
+}
+
+function swap(value, hasher) {
+  switch(hasher.name.toLowerCase()) {
+    case "murmurhash":
+    case "murmurhash32":
+    case "murmurhash64x86":
+    case "murmurhash128x86":
       return swap32(value);
-    case "murmurHash64x64":
-    case "murmurHash128x64":
+    case "murmurhash64x64":
+    case "murmurhash128x64":
       return swap64(value);
     default:
       throw new Error("unknown function");
