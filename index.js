859c1a5fdcb521cf0033d6edd083b406bcf7643a
Edvin Aghanian <54423981+overhub-edvin@users.noreply.github.com>
2021-12-09 19:53:16 -0800

    changed import for node-pre-gyp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var binary = require('node-pre-gyp');
+var binary = require('@mapbox/node-pre-gyp');
 var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 module.exports = require(binding_path);
